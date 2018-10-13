69a93e30fdf2d126f673b992e26308cc7f0bdf45
Mauro Porras C <mauroporrasc@gmail.com>
2018-10-12 19:14:07 -0500

    Improve examples

--- examples/index.js
+++ examples/index.js
@@ -1,19 +1,38 @@
 import { VisualiveSession } from '@visualive/collab'
 
-const PROJECT_ID = 'foo'
-const FILE_ID = 'bar'
-const USER_ID = 'abc123'
+const urlParams = new URLSearchParams(window.location.search)
+const projectIdFromUrl = urlParams.get('project-id')
+const fileIdFromUrl = urlParams.get('file-id')
+const roomIdFromUrl = urlParams.get('room-id')
+const tokenFromUrl = urlParams.get('token')
+
+const $receivedMessages = document.getElementById('receivedMessages')
+const $mediaWrapper = document.getElementById('mediaWrapper')
 
 const visualiveSession = new VisualiveSession()
 
 visualiveSession.join({
-  userId: USER_ID,
-  projectId: PROJECT_ID,
-  fileId: FILE_ID,
+  projectId: projectIdFromUrl,
+  fileId: fileIdFromUrl,
+  roomId: roomIdFromUrl,
+  userId: tokenFromUrl,
 })
 
-visualiveSession.on(VisualiveSession.actions.USER_JOINED, sessionInfo => {
-  console.info(sessionInfo)
+document.formSendMessage.addEventListener('submit', e => {
+  e.preventDefault()
+  const $form = e.target
+  visualiveSession.sendTextMessage($form.messageToSend.value)
+  $form.reset()
 })
 
-visualiveSession.sendTextMessage('hello')
+visualiveSession.on(VisualiveSession.actions.TEXT_MESSAGE, message => {
+  const p = document.createElement('p')
+  p.innerHTML = `<strong>${message.userId}:</strong> ${message.payload.text}`
+  $receivedMessages.appendChild(p)
+})
+
+visualiveSession.on(VisualiveSession.actions.USER_JOINED, message => {
+  const p = document.createElement('p')
+  p.innerHTML = `<strong>(User Joined: ${message.userId})</strong>`
+  $receivedMessages.appendChild(p)
+})
--- src/VisualiveSession.js
+++ src/VisualiveSession.js
@@ -1,39 +1,80 @@
 import io from 'socket.io-client'
+import uuidv4 from 'uuid/v4'
 
 class VisualiveSession {
   constructor() {
-    this.socket = io.connect('https://apistage.visualive.io')
-
-    this.clients = []
-    this.callbacks = Object.keys(VisualiveSession.actions).reduce(
-      (acc, cur) => {
-        return Object.assign({}, { [cur]: [] })
-      },
-      {}
-    )
+    this.callbacks = {}
   }
 
-  join(userInfo) {
-    this.socket.emit(VisualiveSession.actions.USER_JOINED, userInfo)
-  }
+  join(sessionInfo) {
+    const { projectId, fileId, roomId } = sessionInfo
 
-  emit(key, data) {}
+    this.userId = sessionInfo.userId
+    this.fullRoomId = `${projectId}${fileId}${roomId || ''}`
 
-  on(key, callback) {
-    this.socket.on(key, data => {
-      callback(data)
+    this.phone = PHONE({
+      media: { audio: false, video: false },
+      number: this.fullRoomId,
+      publish_key: 'pub-c-c632ffe7-eecd-4ad0-8cc2-6ecc47c17625',
+      subscribe_key: 'sub-c-78f93af8-cc85-11e8-bbf2-f202706b73e5',
+    })
+
+    this.phone.ready(() => {
+      this.phone.dial(this.fullRoomId)
+      this.sendUserJoined(this.userId)
+    })
+
+    this.phone.message((session, message) => {
+      const { type: messageType } = message
+      const callbacks = this.callbacks[messageType]
+      if (callbacks) {
+        callbacks.forEach(callback => callback(message))
+      }
     })
   }
 
-  sendTextMessage(textMessage) {
-    this.socket.emit(VisualiveSession.actions.TEXT_MESSAGE, {
-      payload: textMessage,
+  emit(message) {
+    this.phone.send(Object.assign(message, { userId: this.userId }))
+  }
+
+  on(messageType, callback) {
+    const callbacks = this.callbacks[messageType]
+    this.callbacks[messageType] = callbacks
+      ? callbacks.concat(callback)
+      : [callback]
+  }
+
+  sendUserJoined(userId) {
+    this.emit({
+      type: VisualiveSession.actions.USER_JOINED,
+      payload: {
+        userId,
+      },
+    })
+  }
+
+  sendTextMessage(text) {
+    this.emit({
+      type: VisualiveSession.actions.TEXT_MESSAGE,
+      payload: {
+        text,
+      },
+    })
+  }
+
+  sendCommand(command) {
+    this.emit({
+      type: VisualiveSession.actions.COMMAND,
+      payload: {
+        command,
+      },
     })
   }
 }
 
 VisualiveSession.actions = {
   USER_JOINED: 'user-joined',
+  USER_LEFT: 'user-left',
   TEXT_MESSAGE: 'text-message',
   COMMAND: 'command',
 }
