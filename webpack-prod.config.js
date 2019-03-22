0d878a8cc82de73bce990f63e3bfce38018793b0
mauroporras <mauroporrasc@gmail.com>
2019-03-22 08:28:33 -0500

    v0.0.24

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -4,6 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
+    globalObject: 'this',
     path: path.resolve(__dirname, 'dist'),
     filename: 'visualive-collab.js',
     library: 'visualiveCollab',
