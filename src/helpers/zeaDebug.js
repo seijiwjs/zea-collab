758b109c6db2f47991d19bba5033d5083ae300fa
Philip Taylor <phil@zea.live>
2020-07-17 16:37:48 -0400

    fix: addressed thrown exception when using collab as an es6 module loaded directly in the browser.

--- src/helpers/zeaDebug.js
+++ src/helpers/zeaDebug.js
@@ -1,3 +1,13 @@
-const zeaDebug = debug('zea-collab')
+
+
+const zeaDebug = (globalThis.debug) ? debug('zea-collab') : function() {
+  const searchParams = new URLSearchParams(window.location.search)
+  const emit = searchParams.has("zea-collab")
+  return (msg) => {
+    if (emit) {
+      console.log(msg)
+    }
+  }
+}()
 
 export default zeaDebug
