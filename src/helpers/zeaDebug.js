7e9b2aa5f7dcd3859221d20c5f6fda04c9383218
Philip Taylor <phil@zea.live>
2020-08-14 17:10:49 -0400

    removing 'debug' import again.

--- src/helpers/zeaDebug.js
+++ src/helpers/zeaDebug.js
@@ -1,15 +1,19 @@
-import debug from 'debug'
+// Note: for rawimport to work, we cannot have this import here, as it throws an error:
+// Uncaught TypeError: Failed to resolve module specifier "debug". Relative references must start with either "/", "./", or "../".
 
-const zeaDebug = debug('zea-collab')
+// import debug from 'debug'
+// const zeaDebug = debug('zea-collab')
 
-// const zeaDebug = (globalThis.debug) ? debug('zea-collab') : function() {
-//   const searchParams = new URLSearchParams(window.location.search)
-//   const emit = searchParams.has("zea-collab")
-//   return (msg) => {
-//     if (emit) {
-//       console.log(msg)
-//     }
-//   }
-// }()
+const zeaDebug = globalThis.debug
+  ? debug('zea-collab')
+  : (function () {
+      const searchParams = new URLSearchParams(window.location.search)
+      const emit = searchParams.has('zea-collab')
+      return (msg) => {
+        if (emit) {
+          console.log(msg)
+        }
+      }
+    })()
 
 export default zeaDebug
