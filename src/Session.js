390e748b1c43c72a5b0d3080972e206784a300c7
mauroporras <mauroporrasc@gmail.com>
2020-10-09 07:33:46 -0500

    feat: Consider non-browser envs as localhost

--- src/Session.js
+++ src/Session.js
@@ -438,15 +438,17 @@ class Session {
   }
 
   _isLocalHost() {
-    const retval = Boolean(
+    if (!this.envIsBrowser) {
+      return true
+    }
+
+    return Boolean(
       window.location.hostname === 'localhost' ||
         // [::1] is the IPv6 localhost address.
         window.location.hostname === '[::1]' ||
         // 127.0.0.1/8 is considered localhost for IPv4.
         window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
     )
-
-    return retval
   }
 
   _getRandomString() {
